2bd416d6fb75413a29704fece3c11880240d8fce
prakash32596 <prakash32596@gmail.com>
2018-11-26 22:32:03 +0530

    login component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
+import { NgModule  } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+// angular material api
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatGridListModule } from '@angular/material/grid-list';
+import {MatButtonModule} from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material';
+import { MatCardModule } from '@angular/material/card';
+import {MatInputModule } from '@angular/material/input';
+//angular material  api 
 
+import { AppRoutingModule } from './app-routing.module';
+
+// components
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+//components
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, AppRoutingModule,BrowserAnimationsModule,
+    MatButtonModule,MatCheckboxModule,MatCardModule,MatInputModule,MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
