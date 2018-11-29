b15955629c896e75eccdda284809fa40097f4856
mortuz <mortuzalam@gmail.com>
2018-11-29 10:24:24 +0530

    fix layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,29 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 // angular material api
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatGridListModule } from '@angular/material/grid-list';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material';
 import { MatCardModule } from '@angular/material/card';
-import {MatInputModule } from '@angular/material/input';
-//angular material  api 
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+// angular material  api
 
 import { AppRoutingModule } from './app-routing.module';
 
 // components
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
-//components
+// components
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent
-  ],
+  declarations: [AppComponent, LoginComponent],
   imports: [
-    BrowserModule, AppRoutingModule,BrowserAnimationsModule,
-    MatButtonModule,MatCheckboxModule,MatCardModule,MatInputModule,MatGridListModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatInputModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
