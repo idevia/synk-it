4f4f11fc743b1a616cf5c0d4892e8859889b936c
prakash32596 <prakash32596@gmail.com>
2018-12-16 20:11:46 +0530

    comented errors

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output,Input,EventEmitter} from '@angular/core';
+
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
+  // public hideMe:boolean;
+  public visible: boolean = true;
+  @Output() open: EventEmitter<any> = new EventEmitter();
+  @Output() close: EventEmitter<any> = new EventEmitter();
+  // @Output() sideEvent: EventEmitter<any> =new EventEmitter();
+  // public sidenav ='sending message from nav component';
   constructor() { }
 
   ngOnInit() {
+
   }
 
+  toogleSidenav(){
+    // this.open.emit(this.sideEvent);
+
+      this.visible = !this.visible;
+      if (this.visible) {
+        this.open.emit(null);//open undefinde
+      } else {
+        this.close.emit(null);
+      }
+    
+  }
+  
 }
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, ViewChild,OnInit,HostListener,Output,EventEmitter} from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit{
+  // @Input() message:string;
+  // public sidenav:string;
+  public visible: boolean = true ;
 
+    @ViewChild('side') side: MatSidenavModule;
   constructor() { }
 
   ngOnInit() {
   }
-
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    if (event.target.innerWidth < 800) {
+      this.side.close('side');//close undefined
+    }
+    if  (event.target.innerWidth > 800) {
+      this.side.open('side');//open undefined
+    }
+  }
 }
